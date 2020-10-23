f269b49cda56d10f89ced5a39a7ffb0aae16aaaa
Showrin <showrinece15@gmail.com>
2020-10-24 03:50:48 +0600

    Setup firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC1FysBYPiCrzQ1EosttWJoXCcwxgeXef0",
+  authDomain: "clone-863e2.firebaseapp.com",
+  databaseURL: "https://clone-863e2.firebaseio.com",
+  projectId: "clone-863e2",
+  storageBucket: "clone-863e2.appspot.com",
+  messagingSenderId: "70448669229",
+  appId: "1:70448669229:web:582fb65313d0ec3ab00ea4",
+  measurementId: "G-Y4HD67BQCR",
+};
